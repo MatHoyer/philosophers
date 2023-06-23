d15cf9f5040f73d5f89926abb89009dabf028f36
Mhoyer <mathieuhoyer@gmail.com>
2023-06-23 18:50:50 +0200

    maj

--- philo/sources/main.c
+++ philo/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: mat <mat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 10:58:27 by mhoyer            #+#    #+#             */
-/*   Updated: 2023/06/23 18:50:20 by mat              ###   ########.fr       */
+/*   Updated: 2023/06/23 18:50:47 by mat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@ int main(int ac, char **av)
 {
 	t_simu	simu;
 
-
-
-	tewst;
-	
 	if (ac < 5)
 		exit(printf("Error : ./philo needs at least 5 arguments.\n"));
 	init(&simu, ac, av);
